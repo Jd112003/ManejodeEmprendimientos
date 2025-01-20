d3528ad1472c540b9f8d733a25fe8e1475451a96
Mateo Vivanco Apolo <96545685+Poshoklensio@users.noreply.github.com>
2025-01-20 15:05:18 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Main
+public class Main {
+    public static void main(String[] args) {
+        ProyectoInnovacion proyecto = new ProyectoInvestigacion("AI para Agricultura", "Ana Torres", 50000, "Uso de drones en cultivos");
+        System.out.println(proyecto.resumen());
+    }
+}
